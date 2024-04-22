3298d81f9c208f0e274cdbd379ecd7a25400aa43
Mbah Neilvani <neilvanimbah@gmail.com>
2024-04-22 20:31:48 +0100

    added a code

--- Js/Power of two.js
+++ Js/Power of two.js
@@ -9,3 +9,14 @@ function powerOfTwo(n){
     return num;
 }
 
+// another
+
+function powersOfTwo(n){
+    let arr = [];
+    for(let i = 0; i <= n; i++) {
+      arr.push(2 ** i);
+    }
+    return arr;
+  }
+
+  
