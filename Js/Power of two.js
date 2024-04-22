5fb7125bf418a03ea30b6ceb2efaf3f4fd91dc03
Mbah Neilvani <neilvanimbah@gmail.com>
2024-04-22 20:24:16 +0100

    power of two

---
+++ Js/Power of two.js
@@ -0,0 +1,11 @@
+//Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+function powerOfTwo(n){
+    let num = []
+    for (let i = 0; i <= n; i++ ){
+        num.push(Math.pow(2, i))
+    }
+
+    return num;
+}
+
